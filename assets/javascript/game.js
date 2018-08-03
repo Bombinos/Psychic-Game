0e5529c26d61805493326145d0307f00558ec169
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-08-03 06:02:56 -0600

    Psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,43 @@
      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
      var wins = 0;
      var losses = 0;
+     var gLeft = 10;
+     var guesses = [];
     
      document.onkeyup = function(event) {
-     console.log();
      
      var userGuess = event.key;  
-     
      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        
-     var html =
+     var hiyocaiundai =
      "<p>You chose: " + userGuess + "</p>" +
      "<p>The computer chose: " + computerGuess + "</p>" +
      "<p>wins: " + wins + "</p>" +
-     "<p>losses: " + losses + "</p>";
+     "<p>losses: " + losses + "</p>" + "<p>Guesses left: " + gLeft + "</p>" + "<p>Guesses made: " + guesses + "</p";
 
-            if (userGuess === computerGuess) {
-                wins++;
-            }
-            else {
+     var reset = function() {
+        var gLeft = 10;
+        var guesses = [];
+     }
+
+            if (gLeft < 1) {
                 losses++;
-            };
+                gLeft = 10;
+                guesses = [""];
+            }
+            else if (userGuess == computerGuess) {
+                wins++;
+                gLeft = 10;
+                guesses = [""];
+            }else {
+                gLeft--;
+            }
 
-        
-        // document.querySelector("#Gary").innerHTML = hiyocaiundai
-
+            
+            guesses.push(userGuess)  ;
+            // document.querySelector("#Gary").innerHTML = hiyocaiundai
     
-        document.querySelector("#Gary").innerHTML = html;
-  }
-
+        
+            document.querySelector("#Gary").innerHTML = hiyocaiundai;
+     
+        }
+    
