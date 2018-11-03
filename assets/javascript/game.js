c03b19e1a6736c2dd788c624bbe8e8755adba09c
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-11-03 04:49:28 -0600

    reworking display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,56 @@
      var computerChoices = ["a", "b", "c", "d", "e"];
      var wins = 0;
      var losses = 0;
-     var gLeft = 10;
+     var gsLeft = 2;
      var guesses = [];
-     var key = 
-    
+     var computerGuess = null;
 
-     document.addEventListener('keydown', function(event) {
-        if ((event.code == 'KeyA') || (event.code == 'KeyB') || (event.code == 'KeyC') || (event.code == 'KeyD') || (event.code == 'KeyE')) { 
-
-     var userGuess = event.key.toUpperCase();
-     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-     var hiyocaiundai =
-     "<p>You chose: " + userGuess + "</p>" +
-     "<p>The computer chose: " + computerGuess + "</p>" +
-     "<p>wins: " + wins + "</p>" +
-     "<p>losses: " + losses + "</p>" + "<p>Guesses left: " + gLeft + "</p>" + "<p>Guesses made: " + guesses + "</p";
-
-     
-     
-     
-
+     var updateComputerGuess = function() {
+         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+     };
 
      var reset = function() {
-        var gLeft = 10;
-        var guesses = [];
-     }
+         gsLeft = 2;
+         guesses = [];
+         updateComputerGuess();
+        };
+    
+        updateComputerGuess();
 
-            if (gLeft < 1) {
-                losses++;
-                gLeft = 10;
-                guesses = [""];
-            }
-            else if (userGuess === computerGuess) {
-                wins++;
-                gLeft = 10;
-                guesses = [""];
-            }else {
-                gLeft--;
-            }
+document.addEventListener('keydown', function (event) {
+    if ((event.code == 'KeyA') || (event.code == 'KeyB') || (event.code == 'KeyC') || (event.code == 'KeyD') || (event.code == 'KeyE')) {
+        var userGuess = event.key;
+        
+        
 
-            
-    guesses.push(userGuess);
-   
-    document.querySelector("#compScreen").innerHTML = userGuess;
+        if (userGuess == computerGuess) {
+            wins++;
+            reset();
+        }
 
-    document.querySelector("#Gary").innerHTML = hiyocaiundai;
+        if (gsLeft == 0) {
+            losses++;
+            reset();
+        }
 
-    }else{
+        gsLeft--
+        guesses.push(userGuess);
+
+        document.querySelector("#compScreen").innerHTML = userGuess.toUpperCase();
+
+        // document.querySelector("#Gary").innerHTML = hiyocaiundai;
+
+    } else {
         console.log("wrong key")
     }
 
-     });
+});
+
+// var hiyocaiundai =
+//             "<p>You chose: " + userGuess + "</p>" +
+//             "<p>The computer chose: " + computerGuess + "</p>" +
+//             "<p>wins: " + wins + "</p>" +
+//             "<p>losses: " + losses + "</p>" + "<p>Guesses left: " + gsLeft + "</p>" + "<p>Guesses made: " + guesses + "</p";
 
 
 
